ece75f09494977b985a3096ff2aa6a1fa209fff6
tanquangtruong <39332806+tqtkamitan@users.noreply.github.com>
2021-03-12 12:30:56 +0700

    load api continuouslly

--- main.js
+++ main.js
@@ -1,29 +1,37 @@
 let apiKey= '3ldCW376VG9M94s2sPXZ9ppTmbr56777';
 let imgLimit = 12;
-let giphyAPI = `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}`;
+let offset = 0;
+let giphyAPI = `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=12&offset=${offset}`;
 let displayImgData = [];
 let imgData = [];
 let thisYear = new Date().getFullYear();
 
-function setup(){
+$(document).ready(() => {
+    setup();
+    loadFooter();
+})
 
+function setup(){
+    console.log(giphyAPI)
     fetch(giphyAPI)
     .then(response => {
         return response.json();
     })
     .then(json => {
+        console.log(json)
         imgData = json.data;
-        displayImgData = imgData.slice(0, imgLimit);
+        displayImgData = imgData
+        // displayImgData = imgData.slice(0, imgLimit);
         appendImg();
-        loadFooter();
+        // loadFooter();
     })
 
     .catch(err => console.log(err));
 }
-setup();
 
 function appendImg() {
     displayImgData.map(data => {
+        console.log(data.title)
         var txt1 = `
         <div class='img-container col-lg-3 col-md-4 col-sm-6 col-6'>
             <img class="giphy-img" onclick="showImg('${data.images.original.url}')" src="${data.images.downsized.url}">
@@ -39,9 +47,12 @@ function clearImg() {
 }
 
 function loadMoreImg() {
-    displayImgData = imgData.slice(imgLimit, imgLimit + 12);
-    imgLimit = imgLimit + 12;
-    appendImg()
+    offset = offset + imgLimit;
+    giphyAPI = `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=12&offset=${offset}`;
+    setup();
+    // displayImgData = imgData.slice(imgLimit, imgLimit + 12);
+    // imgLimit = imgLimit + 12;
+    // appendImg()
 }
 
 function showImg(img) {
